8a4fe013bb1b09dc2af8c59f3d310a00d0964029
Alex Amaev <amaevalx@gmail.com>
2025-10-23 16:39:18 +0700

    Init commit

---
+++ src/main/java/com/lakePop/lakePop/LakePopApplication.java
@@ -0,0 +1,13 @@
+package com.lakePop.lakePop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakePopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakePopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakePop/lakePop/LakePopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakePop.lakePop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakePopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
