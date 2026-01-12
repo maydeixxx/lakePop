44f1e0c2dd9657e48765eab8736d3328faeeb268
amaevalx <amaevalx@gmail.com>
2026-01-12 20:16:28 +0700

    Почти готов gateway api

---
+++ gateway/src/test/java/com/lakepop/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakepop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ orderService/src/main/java/com/lakepop/orderService/application/security/SecurityFilter.java
@@ -0,0 +1,4 @@
+package com.lakepop.orderService.application.security;
+
+public class SecurityFilter {
+}
